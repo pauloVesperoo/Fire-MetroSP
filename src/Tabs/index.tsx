0d680ca593433d52bfbf5e609d8b2c3ec94ec5c2
Paulo Vespero <paulovcvespero@gmail.com>
2024-10-23 14:44:46 -0300

    bottomtab consertado

--- src/Tabs/index.tsx
+++ src/Tabs/index.tsx
@@ -1,6 +1,7 @@
 //import { background } from "native-base/lib/typescript/theme/styled-system";
 //import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
 //import Ionicons from 'react-native-vector-icons/Ionicons';
+import AntDesign from '@expo/vector-icons/AntDesign';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Perfil from "./Perfil";
@@ -20,37 +21,36 @@ const tabs = [
     {
       name: 'Principal',
       component: Principal,
-      icon: 'home'
+      icon: 'house'
     },
     {
-        name: 'QRcode',
+        name: 'Camera',
         component: QRcode,
         icon: 'camera'
-      },
+    },
     {
       name: 'Perfil',
       component: Perfil,
       icon: 'person'
     },
   ]
-
-export default function Tabs(){
-    return(
-        <Tab.Navigator screenOptions={screenOptions}>
+  export default function Tabs() {
+    return (
+      <Tab.Navigator screenOptions={screenOptions}>
         {tabs.map((tab) => (
-        <Tab.Screen
-        key={tab.name}
+          <Tab.Screen
+            key={tab.name}
             name={tab.name}
             component={tab.component}
             options={{
-                headerShown: false,
-                tabBarIcon: ({color, size}) => (
-                    <FontAwesome6 name="person-dress" size={24} color="white" />
-            )
-        }}
-        />
-    ))
-}
-        </Tab.Navigator>
-    )
-}
+              headerShown: false,
+              tabBarIcon: ({ color, size }) => (
+                <FontAwesome6 name={tab.icon} size={size} color={color} />
+              )
+            }}
+          />
+        ))}
+      </Tab.Navigator>
+    );
+  }
+  
